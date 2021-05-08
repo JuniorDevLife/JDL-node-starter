2d44385e6659a3c2660ec1952622ebe23f52d0ac
Sean Knowles <s@seanknowles.dev>
2021-05-08 01:22:36 +0100

    standard database to mongo db

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import {port} from "./config.js";
 import Logger from "./core/logger.js";
 import app from './app.js'
 import {connectToMongoDB} from "./database/mongo/index.js"
-import {connectToPostgres} from "./database/postgres/index.js";
-import {connectToCockroachDB} from "./database/cockroachDB/index.js";
 
 app.listen(port, async() => {
   Logger.info(`Server running on port : ${port}`)
   await connectToMongoDB()
-  await connectToPostgres()
-  await connectToCockroachDB()
 }).on('error', (e) => Logger.error(e))
